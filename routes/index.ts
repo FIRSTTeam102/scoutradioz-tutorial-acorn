a1cfa030dd8cf95de43f9e8c9269214d81423aa7
JL102 <jordanlees@mailbox.org>
2024-11-16 13:51:16 -0800

    Update template
    Signed-off-by: JL102 <jordanlees@mailbox.org>

--- routes/index.ts
+++ routes/index.ts
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	// List of JSON elements that will form the scouting form.
 	const myLayout = [
 		{ type: "h2", label: "Autonomous" },
 		{ type: "checkbox", label: "Did they entirely leave Starting Zone?", id: "didStartingZone" },
@@ -41,7 +42,12 @@ router.get('/', function(req, res, next) {
 		{ type: "textblock", label: "Other comments and notes:", id: "otherNotes" }
 	];
 	
+	// Render the webpage.
 	res.render('index', { title: 'Express', myLayout });
 });
+
+router.post('/', function (req, res, next) {
+	console.log('Received form: ', req.body);
+})
 
 module.exports = router;
